323f0ce494e58e4653953d8a62c30b0d7d8eca32
AnBP <anbp@kozo-japan.com>
2024-06-10 22:20:46 +0700

    login register completed

--- app/Http/Controllers/Auth/LoginRegisterController.php
+++ app/Http/Controllers/Auth/LoginRegisterController.php
@@ -3,48 +3,78 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
 use App\Models\User;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class LoginRegisterController extends Controller
 {
     public function register(RegisterRequest $request)
     {
-
+        // create a new user 
         $user =  User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
 
-        $token = $user->createToken($user->email)->plainTextToken;
-        if (!$token) {
-            return response()->json([
-                'status' => 'error',
-                'message' => "register failed",
-            ], 401);
-        }
+        // generate a plan text token 
+        $token = $this->createToken($user);
 
-        // set response
-        $data['user']= $user->makeHidden(['created_at', 'updated_at']);
-        $data['token'] = $token;
-
-        return response()->json([
+        // create the response array
+        $response = [
             'status' => 'success',
             'message' => "register successfully",
-            'data' => $data
-        ]);
+            'data' => [
+                'user' => $user->makeHidden(['created_at', 'updated_at']),
+                'token' => $token,
+            ]
+        ];
+
+        return response()->json($response);
     }
 
-    public function login(Request $req)
+    public function login(LoginRequest $request)
     {
-        if (Auth::attempt(['email' => $req->email, 'password' => $req->password])) {
-            dd("login successful");
-        } else {
-            dd("login failed");
-        };
+        // find the user
+        $user = User::where('email', $request->email)->first();
+
+        // check if user exists
+        if (!$user) {
+            return response()->json([
+                'status' => 'failed',
+                'message' => "User does not exist",
+            ], 404);
+        }
+
+        // check if password is correct
+        if (!Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'status' => 'failed',
+                'message' => "Password is incorrect",
+            ]);
+        }
+
+        // generate a plan text token 
+        $token = $this->createToken($user);
+
+        // create the response array
+        $response = [
+            'status' => 'success',
+            'message' => "Login successfully",
+            'data' => [
+                'token' => $token,
+                'user' => $user->makeHidden(['email_verified_at', 'created_at', 'updated_at']),
+            ]
+        ];
+
+        return response()->json($response);
+    }
+
+
+    private function createToken(User $user): string
+    {
+        return $user->createToken($user->email)->plainTextToken;
     }
 }
